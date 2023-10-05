3fded2a01ad24383c94bca5895a84a74a91f422a
Hanna Cui <ge65tey@mytum.de>
2023-10-05 02:55:39 +0200

    socket connection working

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-server.listen(3001, () => {
+//create new socket connection to client
+io.on("connection", (socket) => {
+  console.log("connected to a client");
+  console.log(socket);
+});
+
+http.listen(3001, () => {
   console.log("listening on *:3001");
 });
